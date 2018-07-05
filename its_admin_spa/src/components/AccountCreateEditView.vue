68e5261bf478fa00c1774ac42bf58f4926909827
phong <tranloiphong2@gmail.com>
2018-07-05 22:07:48 +0700

    Add update acocunt

--- its_admin_spa/src/components/AccountCreateEditView.vue
+++ its_admin_spa/src/components/AccountCreateEditView.vue
@@ -3,29 +3,170 @@
     <v-layout row pa-3>
       <v-flex xs12>
         <span class=title>Account create edit page</span>
+        <v-divider class="my-3"></v-divider>
+        <v-progress-linear v-if="loading.page" color="primary" indeterminate></v-progress-linear>
+        <v-layout column v-else>
+          <v-flex>
+            <div style="width: 300px">
+              <picture-input
+                :prefill="photoPrefill"
+                width="300"
+                height="300"
+                accept="image/jpeg,image/png"
+                size="50"
+                :removable="true"
+                buttonClass="v-btn success"
+                removeButtonClass="v-btn danger"
+                :custom-strings="{
+                  change: 'Đổi hình',
+                  remove: 'xóa',
+                  drag: 'Hình đại diện'
+                }"
+                @change="onChange"
+                @remove="onChange">
+              </picture-input>
+            </div>
+          </v-flex>
+          <v-flex style="width: 25rem">
+            <v-text-field label="Tên" v-model="nameInput"></v-text-field>
+            <v-text-field label="Email" v-model="emailInput"></v-text-field>
+            <v-text-field label="Điện thoại" v-model="phoneInput"></v-text-field>
+            <v-text-field label="Địa chỉ" v-model="addressInput"></v-text-field>
+            <v-text-field label="Ngày sinh" v-model="birthdateInput"></v-text-field>
+          </v-flex>
 
+          <v-flex>
+            <v-btn color="primary"
+                   v-if="mode == 'create'"
+                   @click="onCreateClick">
+              Tạo mới
+            </v-btn>
+            <v-btn color="success"
+                   v-else
+                   :loading="this.loading.updateBtn"
+                   @click="onUpdateClick">
+              Cập nhật
+            </v-btn>
+            <v-btn color="secondary"
+                   @click="onExitClick">
+              Thoát
+            </v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+  import PictureInput from 'vue-picture-input'
+
   export default {
     name: "AccountCreateEditView",
+    components: {
+      PictureInput
+    },
     data() {
       return {
+        loading: {
+          page: true,
+          updateBtn: false
+        },
         mode: 'create',
-        accountId: ''
+        accountId: '',
+        account: undefined,
+        nameInput: '',
+        emailInput: '',
+        addressInput: '',
+        phoneInput: '',
+        birthdateInput: '',
+        photoInput: '',
+        // set image to this when want to add picture
+        photoPrefill: undefined
       }
     },
     created() {
       if (this.$route.name === 'AccountEdit') {
         if (this.$route.query) {
           this.accountId = this.$route.query.id;
-          this.mode = 'edit'
-        }else{
+          this.mode = 'edit';
+          this.$store.dispatch('account/getById', {
+            id: this.accountId
+          })
+            .then(value => {
+              this.account = value;
+              this.fillInputs()
+                .then(() => {
+                  this.loading.page = false;
+                })
+
+            })
+        } else {
           //TODO some error here when no id / wrong id
         }
+      } else {
+        this.loading.page = false;
+      }
+    },
+    methods: {
+      async fillInputs() {
+        return new Promise((resolve, reject) => {
+          if (this.mode == 'edit' && this.account) {
+            this.nameInput = this.account.name;
+            this.emailInput = this.account.email;
+            this.addressInput = this.account.address;
+            this.birthdateInput = this.account.birthdate;
+            this.phoneInput = this.account.phone;
+            if (this.account.photo) {
+              this.photoInput = this.account.photo;
+              this.base64toFile(this.account.photo)
+                .then(value => {
+                  this.photoPrefill = value;
+                  resolve();
+                })
+            } else {
+              resolve();
+            }
+          } else {
+            reject();
+          }
+        })
+      },
+      onChange(image) {
+        this.photoInput = image;
+      },
+      onCreateClick() {
+
+      },
+      onUpdateClick() {
+        this.loading.updateBtn = true;
+        this.$store.dispatch('account/update', {
+          id: this.accountId,
+          name: this.nameInput,
+          email: this.emailInput,
+          phone: this.phoneInput,
+          address: this.addressInput,
+          birthdate: this.birthdateInput,
+          photo: this.photoInput
+        })
+          .then(value => {
+            this.loading.updateBtn = false
+          })
+      },
+      onExitClick() {
+        this.$router.back();
+      },
+      base64toFile(url, filename = 'randomname', mimeType) {
+
+        mimeType = mimeType || (url.match(/^data:([^;]+);/) || '')[1];
+        return (fetch(url)
+            .then(function (res) {
+              return res.arrayBuffer();
+            })
+            .then(function (buf) {
+              return new File([buf], filename, {type: mimeType});
+            })
+        );
       }
     }
   }
--- its_admin_spa/src/store/modules/account.js
+++ its_admin_spa/src/store/modules/account.js
@@ -1,97 +1,96 @@
+const _accounts = [{
+  "id": 1,
+  "name": "Lou Bolderoe",
+  "email": "lbolderoe0@msu.edu",
+  "phone": "453-765-1305",
+  "birthdate": "04/15/1950",
+  "address": "9 Artisan Court aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+}, {
+  "id": 2,
+  "name": "Leighton Berringer",
+  "email": "lberringer1@pcworld.com",
+  "phone": "390-924-2762",
+  "birthdate": "07/11/1976",
+  "address": "4 Arizona Alley"
+}, {
+  "id": 3,
+  "name": "Leanor Everix",
+  "email": "leverix2@youtu.be",
+  "phone": "413-875-6910",
+  "birthdate": "04/18/1983",
+  "address": "2948 Rutledge Point"
+}, {
+  "id": 4,
+  "name": "Maryjo Vallance",
+  "email": "mvallance3@flavors.me",
+  "phone": "240-572-8041",
+  "birthdate": "07/23/1969",
+  "address": "87883 Lighthouse Bay Pass"
+}, {
+  "id": 5,
+  "name": "Helaine Molyneux",
+  "email": "hmolyneux4@domainmarket.com",
+  "phone": "309-569-9369",
+  "birthdate": "07/17/1948",
+  "address": "01917 Sunnyside Court"
+}, {
+  "id": 6,
+  "name": "Liliane Tamsett",
+  "email": "ltamsett5@storify.com",
+  "phone": "656-793-8138",
+  "birthdate": "05/08/1974",
+  "address": "8963 Sunfield Terrace"
+}, {
+  "id": 7,
+  "name": "Ty Witherow",
+  "email": "twitherow6@networkadvertising.org"
+}, {
+  "id": 8,
+  "name": "Ric Harback",
+  "email": "rharback7@tumblr.com"
+}, {
+  "id": 9,
+  "name": "Felic Cannings",
+  "email": "fcannings8@google.pl",
+  "phone": "599-566-9321",
+  "birthdate": "07/06/1985",
+  "address": "165 Karstens Court"
+}, {
+  "id": 10,
+  "name": "Rip Willshire",
+  "email": "rwillshire9@berkeley.edu",
+  "phone": "991-473-9616",
+  "birthdate": "08/01/1978",
+  "address": "4 Drewry Hill"
+}, {
+  "id": 11,
+  "name": "Yanaton Huie",
+  "email": "yhuiea@domainmarket.com",
+  "phone": "862-836-6455",
+  "birthdate": "07/14/1976",
+  "address": "78 Basil Park"
+}, {
+  "id": 12,
+  "name": "Hilary Sanford",
+  "email": "hsanfordb@chron.com",
+  "phone": "197-604-7814",
+  "birthdate": "02/28/1968",
+  "address": "0 Valley Edge Junction"
+}, {
+  "id": 13,
+  "name": "Jesselyn Swiffin",
+  "email": "jswiffinc@wired.com",
+  "phone": "296-213-7700",
+  "birthdate": "10/11/1992",
+  "address": "5644 Emmet Place"
+}];
+
 export default {
   namespaced: true,
   actions: {
     getAll(context, payload) {
-      let accounts = [{
-        "id": 1,
-        "name": "Lou Bolderoe",
-        "email": "lbolderoe0@msu.edu",
-        "phone": "453-765-1305",
-        "birthdate": "04/15/1950",
-        "address": "9 Artisan Court aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-      }, {
-        "id": 2,
-        "name": "Leighton Berringer",
-        "email": "lberringer1@pcworld.com",
-        "phone": "390-924-2762",
-        "birthdate": "07/11/1976",
-        "address": "4 Arizona Alley"
-      }, {
-        "id": 3,
-        "name": "Leanor Everix",
-        "email": "leverix2@youtu.be",
-        "phone": "413-875-6910",
-        "birthdate": "04/18/1983",
-        "address": "2948 Rutledge Point"
-      }, {
-        "id": 4,
-        "name": "Maryjo Vallance",
-        "email": "mvallance3@flavors.me",
-        "phone": "240-572-8041",
-        "birthdate": "07/23/1969",
-        "address": "87883 Lighthouse Bay Pass"
-      }, {
-        "id": 5,
-        "name": "Helaine Molyneux",
-        "email": "hmolyneux4@domainmarket.com",
-        "phone": "309-569-9369",
-        "birthdate": "07/17/1948",
-        "address": "01917 Sunnyside Court"
-      }, {
-        "id": 6,
-        "name": "Liliane Tamsett",
-        "email": "ltamsett5@storify.com",
-        "phone": "656-793-8138",
-        "birthdate": "05/08/1974",
-        "address": "8963 Sunfield Terrace"
-      }, {
-        "id": 7,
-        "name": "Ty Witherow",
-        "email": "twitherow6@networkadvertising.org"
-      }, {
-        "id": 8,
-        "name": "Ric Harback",
-        "email": "rharback7@tumblr.com"
-      }, {
-        "id": 9,
-        "name": "Felic Cannings",
-        "email": "fcannings8@google.pl",
-        "phone": "599-566-9321",
-        "birthdate": "07/06/1985",
-        "address": "165 Karstens Court"
-      }, {
-        "id": 10,
-        "name": "Rip Willshire",
-        "email": "rwillshire9@berkeley.edu",
-        "phone": "991-473-9616",
-        "birthdate": "08/01/1978",
-        "address": "4 Drewry Hill"
-      }, {
-        "id": 11,
-        "name": "Yanaton Huie",
-        "email": "yhuiea@domainmarket.com",
-        "phone": "862-836-6455",
-        "birthdate": "07/14/1976",
-        "address": "78 Basil Park"
-      }, {
-        "id": 12,
-        "name": "Hilary Sanford",
-        "email": "hsanfordb@chron.com",
-        "phone": "197-604-7814",
-        "birthdate": "02/28/1968",
-        "address": "0 Valley Edge Junction"
-      }, {
-        "id": 13,
-        "name": "Jesselyn Swiffin",
-        "email": "jswiffinc@wired.com",
-        "phone": "296-213-7700",
-        "birthdate": "10/11/1992",
-        "address": "5644 Emmet Place"
-      }];
-
-      let total = accounts.length;
-
-      accounts = accounts.filter(account => {
+      let total = _accounts.length;
+      let accounts = _accounts.filter(account => {
         return account.name.indexOf(payload.search) >= 0 ||
           account.email.indexOf(payload.search) >= 0 ||
           account.phone.indexOf(payload.search) >= 0 ||
@@ -126,6 +125,28 @@ export default {
             accounts,
             total
           });
+        }, 2000)
+      })
+    },
+    getById(context, payload) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let account = _accounts.find(acc => payload.id == acc.id);
+          resolve(account);
+        }, 1000)
+      })
+    },
+    update(context, payload) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let account = _accounts.find(acc => payload.id == acc.id);
+          account.photo = payload.photo;
+          account.name = payload.name;
+          account.email = payload.email;
+          account.phone = payload.phone;
+          account.birthdate = payload.birthdate;
+          account.address = payload.address;
+          resolve();
         }, 1500)
       })
     }
