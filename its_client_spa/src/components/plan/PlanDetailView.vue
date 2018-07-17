6ed9feddd1b79ab8c68765c65964bc73fc458422
phong <tranloiphong2@gmail.com>
2018-07-17 22:13:16 +0700

    basic of Plan detail

--- its_client_spa/src/components/plan/PlanDetailView.vue
+++ its_client_spa/src/components/plan/PlanDetailView.vue
@@ -8,8 +8,35 @@
       </v-layout>
     </v-toolbar>
     <v-layout column>
-      <v-flex v-for="location in plan.locations" :key="location.id">
+      <v-flex class="title" my-2>
+        Chưa lên lịch
+      </v-flex>
+      <v-flex elevation-1 my-1>
+        <v-layout column>
+          <v-flex class="subheading font-weight-medium">
+            SADASD
+          </v-flex>
+          <v-divider></v-divider>
+          <v-flex class="body-1" px-2>
+            SADASD
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex v-for="location in plan.locations"
+              :key="location.id"
+              elevation-1 my-1>
         <LocationFullWidth v-bind="location"/>
+      </v-flex>
+      <v-flex class="title" my-2>
+        Ngày 1
+      </v-flex>
+      <v-flex v-for="location in plan.locations"
+              :key="location.id"
+              elevation-1 my-1>
+        <LocationFullWidth v-bind="location"/>
+      </v-flex>
+      <v-flex style="height: 15vh">
+        <!--Holder-->
       </v-flex>
     </v-layout>
   </v-content>
