a62d63e944f1612aeff56ac2d4453e47ee36a5e0
quocbao2411 <39209368+quocbao2411@users.noreply.github.com>
2018-07-24 16:19:50 +0700

    Merge branch 'master' of C:\Users\Admin\Desktop\git\its\ITS with conflicts.

---
+++ its_client_spa/src/components/location/ReviewWritingView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-content>
+    <v-layout column>
+      <v-flex class="headline">
+        Viết bình luận
+      </v-flex>
+      <v-flex>
+
+      </v-flex>
+    </v-layout>
+  </v-content>
+</template>
+
+<script>
+  export default {
+    name: "ReviewWritingView"
+  }
+</script>
+<style scoped>
+
+</style>
