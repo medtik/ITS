f79c25b0e6d56020c929c24ffd6cface499bb1b6
quocbao2411 <39209368+quocbao2411@users.noreply.github.com>
2018-08-01 16:21:58 +0700

    fix bug account siginin

--- its_client_spa/src/store/modules/account.js
+++ its_client_spa/src/store/modules/account.js
@@ -90,10 +90,10 @@ export default {
 
       return new Promise((resolve, reject) => {
         axiosInstance.post('/api/account', {
-          email: email,
+          emailAddress: email,
           password: password,
           rePassword: rePassword,
-          name: name,
+          fullName: name,
           address: address,
           phone: phone,
           birthdate: birthdate,
