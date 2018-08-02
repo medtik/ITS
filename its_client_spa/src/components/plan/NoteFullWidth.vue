bb4ed4446f14afa7d45c2d49bd41350911fc797f
phong <tranloiphong2@gmail.com>
2018-08-02 17:49:33 +0700

    refactor note

--- its_client_spa/src/components/plan/NoteFullWidth.vue
+++ its_client_spa/src/components/plan/NoteFullWidth.vue
@@ -1,21 +1,26 @@
 <template>
-  <v-layout row>
-    <!--HANDLE-->
-    <v-flex xs3
-            sm1
-            d-flex
-            align-center
-            justify-center
-            v-if="$slots.handle"
-            class="text-xs-center">
-      <slot name="handle"></slot>
+  <v-layout row style="max-height: 200px; height: 100px">
+    <!--PHOTO-->
+    <v-flex style="flex-grow: 0" xs4 lg2>
+      <v-layout shrink align-center column>
+        <v-flex>
+          <v-avatar :size="65">
+            <img src="https://picsum.photos/200">
+          </v-avatar>
+        </v-flex>
+        <v-flex mt-1>
+          Phong
+        </v-flex>
+      </v-layout>
+    </v-flex>
+    <v-flex shrink>
+      <v-divider vertical></v-divider>
     </v-flex>
     <!--CONTENT-->
-    <v-layout column pa-1>
+    <v-layout column px-1>
       <v-flex class="title font-weight-medium" my-2>
         {{name}}
       </v-flex>
-      <v-divider></v-divider>
       <v-flex class="body-1" pa-1>
         {{description}}
       </v-flex>
@@ -30,7 +35,20 @@
     props: [
       'name',
       'description'
-    ]
+    ],
+    computed: {
+      imageSize() {
+        if (this.isSmallScreen) {
+          return 520
+        } else {
+          return 150
+        }
+      },
+      isSmallScreen() {
+        return this.$vuetify.breakpoint.name === 'xs'
+      },
+    },
+    methods: {}
   }
 </script>
 
