48a7a1ad970bfc30637d0c4af18e57c7ac99e846
loiphong1996 <tranloiphong2@gmail.com>
2018-08-02 10:25:42 +0700

    location detail hot fix

--- its_admin_spa/src/common/util/axiosInstance.js
+++ its_admin_spa/src/common/util/axiosInstance.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: 'https://some-domain.com/api/',
   baseURL: 'http://itssolution.azurewebsites.net',
   // baseURL: 'http://localhost:56288/',
-  timeout: 10000,
+  timeout: 30000,
 });
 
 export default instance;
