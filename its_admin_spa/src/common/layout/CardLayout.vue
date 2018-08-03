ec6f67bdcfb0b3b17d0f6d33a2e313cefa22c7cf
phong <tranloiphong2@gmail.com>
2018-08-03 09:50:45 +0700

    Add note to git

---
+++ its_admin_spa/src/common/layout/CardLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <div>
+    </div>
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "CardLayout",
+    computed: {
+      layout(){
+
+      }
+    }
+  }
+</script>
