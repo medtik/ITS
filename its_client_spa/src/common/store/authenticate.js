44b4be628e8adbce62d0b6a3a89d25647a2a84de
quocbao2411 <39209368+quocbao2411@users.noreply.github.com>
2018-08-25 11:14:35 +0700

    .

--- its_client_spa/src/common/store/authenticate.js
+++ its_client_spa/src/common/store/authenticate.js
@@ -2,7 +2,9 @@ import axiosInstance from "../util/axiosInstance";
 import RNMsgChannel from 'react-native-webview-messaging';
 import moment from "moment";
 import Raven from "raven-js"
+
 const root = "http://itssolutiong8.azurewebsites.net/";
+
 
 export default {
   namespaced: true,
