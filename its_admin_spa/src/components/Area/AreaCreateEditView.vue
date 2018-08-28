9c602a9860b8fce4c5834926d2f313f6f3a2ec8e
loiphong1996 <tranloiphong2@gmail.com>
2018-08-29 03:56:03 +0700

    Fix edit location

--- its_admin_spa/src/components/Area/AreaCreateEditView.vue
+++ its_admin_spa/src/components/Area/AreaCreateEditView.vue
@@ -19,7 +19,7 @@
             <v-layout row style="justify-items: end">
               <v-flex>
                 <v-select label="Tìm câu hỏi"
-                          :items="['ques 1', 'ques 2']"/>
+                          :items="allQuestions"/>
               </v-flex>
               <v-flex d-flex style="flex-grow: 0;align-items: center">
                 <v-btn flat color="green">
@@ -27,27 +27,6 @@
                 </v-btn>
               </v-flex>
             </v-layout>
-            <v-data-table
-              :headers="[
-                {text: 'Nội dung', value: 'text'},
-                {text: 'Thể loại', value: 'category'},
-                {text: 'Số câu trả lời', value: 'answers.length'},
-                {text: 'Hành động', value: 'id', sortable: false},
-              ]"
-              :items="choosenQuestions">
-              <template
-                slot="items"
-                slot-scope="props">
-                <td>{{props.item.text}}</td>
-                <td>{{props.item.category}}</td>
-                <td>{{props.item.answerCount}}</td>
-                <td>
-                  <v-btn icon flat color="red">
-                    <v-icon color="red">delete</v-icon>
-                  </v-btn>
-                </td>
-              </template>
-            </v-data-table>
             <!--<v-flex>-->
               <!--<v-btn v-if="mode == 'create'" color="primary">-->
                 <!--Tạo-->
@@ -75,6 +54,7 @@
     ErrorDialog,
     SuccessDialog
   } from "../../common/block";
+  import mapState from "vuex"
 
   export default {
     name: "AreaCreateEditView",
